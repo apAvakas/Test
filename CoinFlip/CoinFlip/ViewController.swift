7e2fd746cbc93c8381a95116579e3536ab7e18e6
Adrianna Payne <Adrianna@Adriannas-MBP.home>
2017-10-30 11:02:14 -0700

    Update with Print

--- CoinFlip/CoinFlip/ViewController.swift
+++ CoinFlip/CoinFlip/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         
         let title = sender.title(for: .normal)
         //decision.text = "You clicked \(title ?? "")"
+        print("Hello World")
         
         if computerChoice.text == title {
             decision.text = "Tie"
